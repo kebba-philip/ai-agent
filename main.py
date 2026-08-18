dc80bab630ecec8c6cb1b7c4d67e3d3275426e76
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-18 18:44:50 +0000

    verbose output

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
+
 def main():
     load_dotenv()
+
     api_key = os.environ.get("OPENROUTER_API_KEY")
 
     if api_key is None:
@@ -14,17 +17,44 @@ def main():
         api_key=api_key,
     )
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+
+    parser.add_argument(
+        "user_prompt",
+        type=str,
+        help="User prompt"
+    )
+
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Enable verbose output"
+    )
+
+    args = parser.parse_args()
+
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": args.user_prompt,
             }
-        ]
+        ],
     )
 
+    if response.usage is None:
+        raise RuntimeError(
+            "API response did not include token usage information."
+        )
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+
     print(response.choices[0].message.content)
+
 
 if __name__ == "__main__":
     main()
