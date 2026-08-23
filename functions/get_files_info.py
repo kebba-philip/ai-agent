3de0eba9dd572b6ebfa7988b2dc4a08e4ac2f06d
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-23 15:55:02 +0000

    list file contents

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,20 +1,16 @@
 import os
 
-
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
-        working_dir_abs = os.path.abspath(working_directory)
+        abs_working_dir = os.path.abspath(working_directory)
 
         target_dir = os.path.normpath(
-            os.path.join(working_dir_abs, directory)
+            os.path.join(abs_working_dir, directory)
         )
 
-        valid_target_dir = (
-            os.path.commonpath([working_dir_abs, target_dir])
-            == working_dir_abs
-        )
-
-        if not valid_target_dir:
+        if os.path.commonpath(
+            [abs_working_dir, target_dir]
+        ) != abs_working_dir:
             return (
                 f'Error: Cannot list "{directory}" '
                 "as it is outside the permitted working directory"
@@ -23,7 +19,20 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
 
-        return f'Success: "{directory}" is within the working directory'
+        final_response = ""
+        contents = os.listdir(target_dir)
+
+        for content in contents:
+            content_path = os.path.join(target_dir, content)
+            is_dir = os.path.isdir(content_path)
+            size = os.path.getsize(content_path)
+
+            final_response += (
+                f"- {content}: file_size={size} bytes, "
+                f"is_dir={is_dir}\n"
+            )
+
+        return final_response
 
     except Exception as e:
         return f"Error: {e}"
