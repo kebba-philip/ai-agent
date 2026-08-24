5704d4cbfa8ae27a524ece2ff60b527edb9c278e
KebbaPhilip <kebbaphilip@gmail.com>
2026-08-24 07:05:44 +0000

    update llm respond

--- prompts.py
+++ prompts.py
@@ -1,5 +1,9 @@
 system_prompt = """
-You are a robot.
-Regardless of what the user asks, respond with exactly:
-"I'M JUST A ROBOT"
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
